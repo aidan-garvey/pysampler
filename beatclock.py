29be6e1c047da66c602b611cab40ee32c87b8fb1
Aidan Garvey <AidanGarvey@pm.me>
2022-12-11 19:39:25 -0600

    Rewrite sampler and beatclock

--- beatclock.py
+++ beatclock.py
@@ -1,35 +1,76 @@
+'''
+beatclock.py
+
+Sends clock pulses to output device. Can be stopped and started.
+'''
 
 import time
 import socket
 import mido.ports
 import mido.messages
+from select import select
 
-def beatclock(host, port, midiport: mido.ports.BaseOutput, sec_per_qn, steps):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    message = mido.messages.Message(type='clock')
-    step = 0
-    pulse = 0
-    last_time = time.time()
+PPQN = 24
+PPS = PPQN // 4
+MAX_STEPS = 16
 
-    pcount = 0
+STOP_MSG = 'stop'
+SHUTDOWN_MSG = 'shutdown'
 
-    midiport.send(mido.messages.Message(type='start'))
+class BeatClock:
 
-    try:
-        while True:
+    clock_signal = mido.messages.Message(type='clock')
+    start_signal = mido.messages.Message(type='start')
+    stop_signal = mido.messages.Message(type='stop')
+    sock: socket.socket
+    started = False
+    sec_per_pulse: float
+    midiport: mido.ports.BaseOutput
+
+    def __init__(self, sec_per_pulse: float, addr, midiport):
+        self.sec_per_pulse = sec_per_pulse
+        self.midiport = midiport
+        self.sock = socket.create_connection(addr)
+
+    def run(self):
+        step = 0
+        pulse = 0
+        carry = 0.0
+        self.started = True
+        stopped = False
+        shutdown = False
+
+        # self.midiport.send(self.start_signal)
+        last_time = time.time()
+        while not stopped:
             curr_time = time.time()
-            if curr_time - last_time >= sec_per_qn:
-                midiport.send(message)
+            elapsed = curr_time - last_time + carry
+            while elapsed >= self.sec_per_pulse:
+                # self.midiport.send(self.clock_signal)
                 pulse += 1
-                pcount += 1
-                if pulse == 24:
-                    sock.sendto(bytes(str(step), 'utf-8'), (host, port))
+                if pulse == PPS:
+                    self.sock.send(bytes(str(step), 'utf-8'))
                     pulse = 0
                     step += 1
-                    step %= steps
-                last_time = curr_time
-    except KeyboardInterrupt:
-        pass
-    except Exception as e:
-        print(e)
-    print(pcount)
+                    step %= MAX_STEPS
+                elapsed -= self.sec_per_pulse
+
+            readlist, _, _ = select([self.sock], [], [], 0)
+            for readable in readlist:
+                msg = readable.recv(1024).decode('utf-8')
+                if msg == STOP_MSG:
+                    stopped = True
+                elif msg == SHUTDOWN_MSG:
+                    stopped = True
+                    shutdown = True
+
+            carry = elapsed
+            last_time = curr_time
+        
+        # self.midiport.send(self.stop_signal)
+        if shutdown:
+            self.shut_down()
+
+    def shut_down(self):
+        self.sock.close()
+        # self.midiport.close()
--- sampler.py
+++ sampler.py
@@ -1,92 +1,78 @@
 
-# add script's directory to PATH to detect libwinmedia.dll
-import os
-os.environ["PATH"] = os.path.dirname(__file__) + os.pathsep + os.environ["PATH"]
-
-from sys import argv, stdout
-import time
-import threading
-import socket
-# import libwinmedia as lwm
-import mido
+import keyboard
 import beatclock
+import socket
+import json
+import mido
+import threading
+from sys import argv, stdout
+from select import select
 
+HOST = 'localhost'
+BACKEND = 'mido.backends.rtmidi'
+CONFIG = json.loads(open('config.json', 'r').read())
+STOP_MSG = b'stop'
+SHUTDOWN_MSG = b'shutdown'
+
+class sampler:
+    sec_per_pulse: float
+    midiport: mido.ports.BaseOutput | None
+    server: socket.socket
+    clock: beatclock.BeatClock
+    clocksock: socket.socket
+    clockaddr: tuple
+    clockthread: threading.Thread
+    online: bool
+
+    def __init__(self):
+        self.online = True
+        self.sec_per_pulse = (60 / CONFIG['bpm']) / 24
+        self.midiport = None
+        # devs: list[str] = mido.get_output_names()
+        # for dev in devs:
+        #     if dev.find(CONFIG['device']) >= 0:
+        #         self.midiport = mido.open_output(dev)
+        self.server = socket.create_server((HOST, CONFIG['port']))
+        self.server.listen()
+        self.clock = beatclock.BeatClock(self.sec_per_pulse,
+            (HOST, CONFIG['port']), self.midiport)
+        self.clocksock, self.clockaddr = self.server.accept()
+        self.clockthread = threading.Thread(target=self.clock.run,
+            name='clock_thread', args=(), daemon=True)
+    
+    # listen for keys and socket messages
+    def run(self):
+        print("Ready")
+        keyboard.on_press(self.handle_key)
+        while self.online:
+            r, _, _ = select([self.clocksock], [], [], 0.1)
+            if r:
+                step = int(self.clocksock.recv(1024).decode('utf-8'))
+                if self.online:
+                    print(step + 1, flush=True)
+    
+    def handle_key(self, event: keyboard.KeyboardEvent):
+        if event.name == '=' or event.name == '+':
+            self.clockthread.start()
+        elif event.name == '-' or event.name == '_':
+            self.clocksock.send(STOP_MSG)
+        elif event.name == '\\' or event.name == '|':
+            print("Shutting down...", flush=True)
+            self.online = False
+
+    def shut_down(self):
+        self.clocksock.send(SHUTDOWN_MSG)
+        self.server.close()
+        self.clocksock.close()
+
+# setup beat clock, listen for start keypress, print something with each step
+# signal recieved, listen for stop keypress, quit.
 if __name__ == "__main__":
-    argc = len(argv)
-    bpm = 145
-    max_steps = 16
-    unlit_color = "\x1B[34m"
-    lit_color = "\x1B[36m"
-    quiet = False
-
-    i = 1
-    while (i < argc):
-        if len(argv[i] <= 1):
-            continue
-
-        opt = argv[i][1]
-        has_next = i < argc - 1
-        if opt == 'b' and has_next:
-            bpm = int(argv[i + 1])
-            i += 1
-        elif opt == 's' and has_next:
-            max_steps = int(argv[i + 1])
-            i += 1
-        elif opt == 'q':
-            quiet = True
-
-        i += 1
-    
-    # seconds per beat
-    sps = 60 / bpm
-    sps /= 24 # 24 PPQN
-
-    # set up audio player
-    # player = lwm.Player();
-    # player.open(lwm.Media("E:/Git/personal/sampler/hit.mp3"))
-
-    # set up socket to recieve clock signals
-    clocksock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    host, port = 'localhost', 8000
-    clocksock.bind((host, port))
-
-    # set up midi port for clock to write to
-    mido.set_backend('mido.backends.rtmidi')
-    print(mido.get_output_names())
-    dest = mido.get_output_names()[1]
-
-    # get thread ready to run beat clock
-    clockthread = threading.Thread(target=beatclock.beatclock, name='beatclock',
-        args=(host, port, mido.open_output(dest), sps, max_steps), daemon=True)
-
-    if not quiet:
-        print(f'\nBPM: {bpm}\nSteps: {max_steps}\n\nPress CTRL+C to quit.\n')
-    
-    # draw initial state
-    print(unlit_color)
-    for i in range(max_steps):
-        print("[]", end='')
-    print("\r\x1B[?25l", end='')
-
-    sc = 0
-
-    clockthread.run()
+    print("Starting...")
+    mido.set_backend(BACKEND)
+    s = sampler()
     try:
-        while True:
-            print(lit_color + "[]", end='', flush=True)
-            stdout.flush()
-            step = int(clocksock.recv(256).decode('utf-8'))
-            sc += 1
-            print(unlit_color + "\x1b[2D[]", end='', flush=True)
-            stdout.flush()
-            if step == max_steps-1:
-                print("\x1B[0J\r", end='')
-    except KeyboardInterrupt as kbi:
-        pass
+        s.run()
     except Exception as e:
         print(e)
-
-    print("\x1B[0m\x1B[?25h\nExiting...")
-    print(sc)
-    clocksock.close()
-    exit()
+    s.shut_down()
